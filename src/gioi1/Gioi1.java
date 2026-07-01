b1926cd08f17ea342daa3a7a3a87ada9ae2141c1
LeDacHuy203 <ldh2k3@gmail.com>
2026-07-02 06:20:48 +0700

    first commit

---
+++ src/gioi1/Gioi1.java
@@ -0,0 +1,19 @@
+package gioi1;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Gioi1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String regex = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@#$%^&+=]).{8,}$";
+        System.out.print("Nhập mật khẩu: ");
+        String pass = scanner.nextLine();
+        boolean result = Pattern.matches(regex, pass);
+        if (result) {
+            System.out.println("Mật khẩu hợp lệ");
+        } else {
+            System.out.println("Mật khẩu không hợp lệ");
+        }
+    }
+}
